00a963210f5c3a8dd7ba25c47558a0a0f61fbefc
Eloi Strée <eloistree@gmail.com>
2024-09-07 00:04:54 +0200

    Save: 09/07/2024 12:04 AM

--- Runtime/GUIDResourcesLoaderMono.cs
+++ Runtime/GUIDResourcesLoaderMono.cs
@@ -6,35 +6,40 @@ public class GUIDResourcesLoaderMono : MonoBehaviour
 {
 
     public Transform m_whereToCreateRoot;
-    public GameObject m_currentViewObject;
-
-    public string m_defaultNameToGive = "HeRCArena";
-    public string m_folderGUID="GUID Here";
-
+    public string m_prefabNameGivenToLoad = "Default";
+    public string m_folderNameAsGUID= "7b40a7c4-534b-4f8d-96cf-ccb3cd4031a2";
     public bool m_loadAtAwake;
+
+    [Header("Debugging")]
+    public GameObject m_currentCreatedObject;
+
     void Awake()
     {
         if (m_loadAtAwake) {
-            LoadGUID(m_folderGUID);
+            LoadGUID(m_folderNameAsGUID);
         }
     }
 
     [ContextMenu("Load GUID")]
     public void LoadGUIDFromInspector()
     {
-        LoadGUID(m_folderGUID);
+        LoadGUID(m_folderNameAsGUID);
     }
         public void LoadGUID(string guid)
         {
-            m_folderGUID = guid;
-        GameObject prefab = Resources.Load($"{m_folderGUID}/{m_defaultNameToGive}") as GameObject;
+            m_folderNameAsGUID = guid;
+        GameObject prefab = Resources.Load($"{m_folderNameAsGUID}/{m_prefabNameGivenToLoad}") as GameObject;
         if (prefab != null)
         {
             GameObject created = Instantiate(prefab, transform.position, transform.rotation);
             if (created)
             {
-                if (m_currentViewObject != null)
-                    Destroy(m_currentViewObject);
+                if (m_currentCreatedObject != null)
+                { 
+                    if(Application.isPlaying)
+                    Destroy(m_currentCreatedObject);
+                    else DestroyImmediate(m_currentCreatedObject);
+                }
                 created.transform.parent = m_whereToCreateRoot;
                 created.transform.localRotation = Quaternion.identity;
                 created.transform.localScale = Vector3.one;
@@ -44,9 +49,5 @@ public class GUIDResourcesLoaderMono : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  
 }
